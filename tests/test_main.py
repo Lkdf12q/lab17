8862c8fa98fe43db8e8790e7d6e0f0ed32855c7d
Lkdf12q <nikibrawls@gmail.com>
2025-04-26 09:13:36 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
 import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.main import add
 
 def test_add_positive_numbers():
